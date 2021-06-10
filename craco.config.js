652622eea1362d5c7714ff2243b5fa5ad661fad9
任我行 <skywalk94@foxmail.com>
2021-06-10 09:52:48 +0800

    移除rem

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const CracoLessPlugin = require("craco-less");
-const px2rem = require("postcss-px2rem-exclude");
 module.exports = {
     //antd插件配置，使用less并定义主题
     plugins: [
@@ -21,22 +20,6 @@ module.exports = {
     webpack: {
         alias: {
             "@": path.resolve("src"),
-        },
-    },
-
-    //rem配置
-    style: {
-        postcss: {
-            loaderOptions: {
-                ident: "postcss",
-                plugins: () => [
-                    px2rem({
-                        remUnit: 37.5,
-                        remPrecision: 2,
-                        exclude: /node_modules/i,
-                    }),
-                ],
-            },
         },
     },
 }
