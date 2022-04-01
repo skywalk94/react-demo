460678ef813fc3061944e5712e01b67e4e8e2cda
skywalk94 <skywalk94@foxmail.com>
2022-04-01 18:04:39 +0800

    取参

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Outlet, Link, useNavigate, useLocation, useSearchParams } from "react-router-dom"
+import { Outlet, Link, useNavigate, useSearchParams } from "react-router-dom"
 export default () => {
     let navigate = useNavigate()
-    let location = useLocation()
-    let [searchParams, setSearchParams] = useSearchParams()
-    console.log(searchParams.get("aoe"))
-    // console.log(location.search)
+    let [searchParams] = useSearchParams()
+    console.log(searchParams.get("id")) // 1
     return (
         <div>
             <button onClick={() => navigate(-1)}>后退</button>
-            <br />
             <Link to="/home">左</Link> &nbsp;
             <Link to="/home/home-right">右</Link>
             <Outlet />
